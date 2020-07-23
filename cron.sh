05191744329435fc5d757a98d3cd6f7520ec4d41
Nikhil Kedar <nkedar@nvidia.com>
2020-07-23 10:42:07 +0530

    local change

---
+++ cron.sh
@@ -0,0 +1,13 @@
+git checkout master
+git remote update
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+       
+fi
+echo "check complete "
